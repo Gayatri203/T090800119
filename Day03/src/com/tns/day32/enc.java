e1e5a0a4f67fc73abdf7fc37619379bdaa5375d4
Gayatri <gayatriji002@gmail.com>
2024-07-08 10:55:19 +0530

    tns

---
+++ Day03/src/com/tns/day32/enc.java
@@ -0,0 +1,22 @@
+package com.tns.day32;
+
+public class enc {
+
+	public static void main(String[] args) {
+		
+		//object creation
+				encapsulation obj= new encapsulation();
+				
+				obj.setName("Gayatri ");
+				obj.setAge(21);
+				obj.setSerilaNum(1001);
+				
+				System.out.println(obj.getAge());
+				System.out.println(obj.getName());
+				System.out.println(obj.getSerilaNum());
+				
+				System.out.println(obj);
+				
+	}
+
+}
